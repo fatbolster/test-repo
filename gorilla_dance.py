76e29fea5945e84598572a84c3b52bd8c2a8ecef
Chua Kai Jie, Emerson <emersonchua934@outlook.com>
2026-01-09 21:40:47 +0800

    Gorilla

---
+++ gorilla_dance.py
@@ -0,0 +1,32 @@
+import time
+import itertools
+
+frames = [
+    r"""
+  ,--.   
+ (o_o )  
+ /(  )\\ 
+ /  |  \\ 
+   / \\    
+  /   \\   
+""".strip("\n"),
+    r"""
+  ,--.   
+ (O_o )  
+ /(  )\\ 
+ /  |  \\ 
+  \\ |     
+   / \\    
+""".strip("\n"),
+]
+
+clear = "\033c"
+
+def main() -> None:
+    for frame in itertools.cycle(frames):
+        print(clear + frame)
+        time.sleep(0.3)
+
+
+if __name__ == "__main__":
+    main()
